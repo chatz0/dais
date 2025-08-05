4f3a943b3fafdfe156f0de8b143838f4e56045b3
chatz0 <dimitris.chatzopoulos@ucd.ie>
2025-08-05 15:58:49 +0100

    Fix white page issue: add base href and update Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from './components/Header';
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GraphPage from './components/GraphPage';
 import CVPage from './pages/CVPage';
 import ContactPage from './pages/ContactPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/dais">
       <Header />
       <Routes>
         <Route path="/" element={<GraphPage />} />
