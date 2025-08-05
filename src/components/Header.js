8aa185fa22c050cfa7c45ce11fd30da47addfd44
chatz0 <dimitris.chatzopoulos@ucd.ie>
2025-08-05 16:53:21 +0100

    Switch to CDN for social icons; keep UCD logo local

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,60 @@ const Header = () => {
         />
         <span className="profile-title">Dimitris Chatzopoulos</span>
       </div>
+
+      <div className="social-icons">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/dimhatzo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg"
+            alt="LinkedIn"
+            className="icon"
+          />
+        </a>
+
+        {/* DBLP */}
+        <a
+          href="https://dblp.org/pid/135/6249.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/dblp.svg"
+            alt="DBLP"
+            className="icon"
+          />
+        </a>
+
+        {/* Google Scholar */}
+        <a
+          href="https://scholar.google.com.hk/citations?hl=en&user=vXz1bl4AAAAJ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/googlescholar.svg"
+            alt="Google Scholar"
+            className="icon"
+          />
+        </a>
+
+        {/* UCD logo (local) */}
+        <a
+          href="https://www.ucd.ie/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/icons/ucd.png`}
+            alt="UCD"
+            className="icon"
+          />
+        </a>
+      </div>
     </header>
   );
 };
