db9ca1bfe795011314ff01884f4d5556c1b89ab9
chatz0 <dimitris.chatzopoulos@ucd.ie>
2025-08-05 15:10:00 +0100

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Header from './components/Header';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import GraphPage from './components/GraphPage';
 import CVPage from './pages/CVPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <Router basename="/dais">
-      {/* Header is always visible at the top */}
+    <Router>
       <Header />
-
       <Routes>
         <Route path="/" element={<GraphPage />} />
         <Route path="/cv" element={<CVPage />} />
